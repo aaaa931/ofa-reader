891e035e4e527a59c9ae1b698f6a5bb654b7e3c7
aaaa931 <st876504@gmail.com>
2023-09-25 14:01:58 +0800

    fix: delete not used

--- src/stores/notification.ts
+++ src/stores/notification.ts
@@ -5,9 +5,5 @@ import type { Notification } from '@/interface/notification'
 export const useNotificationStore = defineStore('notification', () => {
   const notification = ref<Notification[]>()
 
-  // function setNotification(newNotification: Notification[]) {
-  //   notification.value = newNotification
-  // }
-
   return { notification }
 })
